07078356b2e8e83484c48979d0322e30d6f71081
lichengming <lichengming@MacBook-Pro-2.local>
2025-08-21 15:30:14 +0800

    perf:优化页面结构

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SideBar from './components/SideBar/index.vue';
+import ChatArea from './components/ChatArea/index.vue';
+</script>
 
 <template>
-    <div class="app-container"></div>
+    <div class="app-container">
+        <SideBar />
+        <ChatArea />
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.app-container {
+    position: relative;
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+}
+</style>
