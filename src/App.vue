ccb8ede681cc656cc4005c6ddcfbce726069dcdc
lichengming <lichengming@MacBook-Pro-2.local>
2025-08-25 11:56:00 +0800

    chore:naiveui全局上下文

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
+import Context from './Context.vue';
 import SideBar from './components/SideBar/index.vue';
 import ChatArea from './components/ChatArea/index.vue';
 </script>
 
 <template>
-    <div class="app-container">
-        <ChatArea />
-        <SideBar />
-    </div>
+    <Context>
+        <div class="app-container">
+            <ChatArea />
+            <SideBar />
+        </div>
+    </Context>
 </template>
 
 <style scoped lang="scss">
---
+++ src/Context.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { NDialogProvider } from 'naive-ui';
+</script>
+
+<template>
+    <n-dialog-provider>
+        <slot />
+    </n-dialog-provider>
+</template>
